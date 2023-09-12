fdc97489c5b6737434a2a7f34cef4f0389e7ea9a
Iamruthm <144849798+Iamruthm@users.noreply.github.com>
2023-09-12 14:27:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit
 streamlit.title ( 'My parents New Healthy Dinner')
 streamlit.header ('New Menu')
-streamlit.text ('- Juice -
-                  Orange
-                  Apple
-                  Mango')
+streamlit.text ('- Juice - Orange, apple, Mango ')
                   
                 
