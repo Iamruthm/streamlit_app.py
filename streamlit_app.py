3edaa13ce95c49945ac290e912da536fef13c4fe
Iamruthm <144849798+Iamruthm@users.noreply.github.com>
2023-09-12 14:28:17 -0700

    Update streamlit_app.py
    Modified changes so they mimic course

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title ( 'My parents New Healthy Dinner')
-streamlit.header ('New Menu')
-streamlit.text ('- Juice - Orange, apple, Mango ')
-                  
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')                  
                 
