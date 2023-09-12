aae593bb23dba640b37f8392fb153b79291d3728
Iamruthm <144849798+Iamruthm@users.noreply.github.com>
2023-09-12 14:26:34 -0700

    Update streamlit_app.py
    Added 'New Menue' and different juice options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
 streamlit.title ( 'My parents New Healthy Dinner')
+streamlit.header ('New Menu')
+streamlit.text ('- Juice -
+                  Orange
+                  Apple
+                  Mango')
+                  
+                
