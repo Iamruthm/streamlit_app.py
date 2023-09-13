21e9cfb2c65b30d2671ca9026036dd893eb0a95e
Iamruthm <144849798+Iamruthm@users.noreply.github.com>
2023-09-13 13:33:23 -0700

    Update streamlit_app.py
    adding panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')    
 streamlit.text ('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-                
+Import Pandas 
+my_fruit_list = pandas.read_csv("https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html")
+streamlit.dataframe(my_fruit_list)
